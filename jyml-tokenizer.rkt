0214ec98162ca4e71e7f8ea38f99a47846ceb64c
梁建溢 <a.124p@live.com>
2017-11-15 15:27:54 +0800

    changesets-03
    修改了 find-tokenregular-whitelist? 和 find-tokenregular-blacklist? 两个过程，并删除了部分测试代码，但 tokenizer 仍然有 bug

--- jyml-tokenizer.rkt
+++ jyml-tokenizer.rkt
@@ -4,25 +4,24 @@
 ;;; 需单个字符作为 token 的字符一律划入白名单
 (define tokenregular-whitelist
   (vector #\( #\)))
+
 (define (find-tokenregular-whitelist? target)
-  (define (stop? arg)
-    (if [= arg (vector-length tokenregular-whitelist)]
-        #f
-        (if [equal? target (vector-ref tokenregular-whitelist arg)]
-            #t
-            #f)))
-  (do ([index 0 (+ index 1)])
-    (stop? index)))
+  (do ([index 0 (+ index 1)]
+       [found? (equal? target (vector-ref tokenregular-whitelist 0))
+               (equal? target (vector-ref tokenregular-whitelist index))])
+    ((or [= index (vector-length tokenregular-whitelist)] found?)
+     found?)))
 
 ;;; 依据黑名单提供的字符作为 seperator
 (define tokenregular-blacklist
   (vector #\space #\newline))
+
 (define (find-tokenregular-blacklist? target)
-  (do ([index 0 (+ index 1)])
-    ((or [= index (vector-length tokenregular-blacklist)]
-         [equal? target (vector-ref tokenregular-blacklist index)])
-     [when [equal? target (vector-ref tokenregular-blacklist index)]
-       #t])))
+  (do ([index 0 (+ index 1)]
+       [found? (equal? target (vector-ref tokenregular-blacklist 0))
+               (equal? target (vector-ref tokenregular-blacklist index))])
+    ((or [= index (vector-length tokenregular-blacklist)] found?)
+     found?)))
 
 ;(define (procedure-table)
 ;  ())
@@ -32,11 +31,30 @@
         [word ""]
         [current-char null])
     (do ([index 0 (+ index 1)])
-      ([= index (string-length exp)])
+      ([= index (string-length exp)] token-sequence)
       (begin
         (set! current-char [string-ref exp index])
         (if (find-tokenregular-whitelist? current-char)
             (set! token-sequence [append-linkedlist token-sequence (mcons current-char null)])
             (if (find-tokenregular-blacklist? current-char)
-                (set! word [string-append word (string current-char)])
-                (set! word "")))))))
+                (set! word "")
+                (set! word [string-append word (string current-char)])))))))
+
+
+;(define (tokenizer-2 exp)
+;  (let ([token-sequence null]
+;        [word ""]
+;        [current-char null]
+;        [whitelist? null]
+;        [blacklist? null])
+;    (do ([index 0 (+ index 1)])
+;      ([= index (string-length exp)] token-sequence)
+;      (begin
+;        (set! current-char [string-ref exp index])
+;        (set! whitelist? (find-tokenregular-whitelist? current-char))
+;        (set! blacklist? (find-tokenregular-blacklist? current-char))
+;        (if (whitelist?)
+;            (set! token-sequence [append-linkedlist token-sequence (mcons current-char null)])
+;            (if (blacklist?)
+;                (set! word "")
+;                (set! word [string-append word (string current-char)])))))))
