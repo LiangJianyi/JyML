629d9b74227ed18a4d900e76e3384703d54c4b66
梁建溢 <a.124p@live.com>
2017-11-19 18:09:56 +0800

    Changesets-06
    在 jyml-ast.rkt 文件中增加了 append-astnode 过程

--- jyml-ast.rkt
+++ jyml-ast.rkt
@@ -1,5 +1,10 @@
 #lang racket
 
+(provide make-ast)
+(provide iterator-ast)
+(provide append-ast)
+(provide append-astnode)
+
 (define (make-ast left right)
   (mcons left right))
 
@@ -16,5 +21,15 @@
           obj
           ast)))
 
+(define (append-astnode ast obj)
+  (if [mpair? ast]
+      (cond [(null? [mcar ast]) (make-ast (append-astnode [mcar ast] obj) [mcdr ast])]
+            [(null? [mcdr ast]) (make-ast [mcar ast] (append-astnode [mcdr ast] obj))]
+            [else ast])
+      (if [null? ast]
+          obj
+          ast)))
+
 ;(define (copy-ast ast)
 ;  ())
+
