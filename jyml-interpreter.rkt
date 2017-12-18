9986791a213b7cc7b31ea681b5f683ad35e30da1
梁建溢 <a.124p@live.com>
2017-12-18 18:36:46 +0800

    Changesets-18
    Add object-table in jyml-interpreter.rkt

--- jyml-interpreter.rkt
+++ jyml-interpreter.rkt
@@ -1,6 +1,22 @@
 #lang racket
 (require liangjianyi-racket/linkedlist)
+(require liangjianyi-racket-sundry/utility)
 (require "./jyml-parser.rkt")
+
+(define (object-table opt enviroment)
+  (define address ((enviroment 'eid)))
+  (define entries null)
+  (define (address-incre!)
+    (let ([incre (substring (+ (string-first-index address #\|) 1)
+                            (string-length address))]
+          [area-code (substring address 0 (+ (string-first-index address #\|) 1))])
+      (set! address (string-append area-code (+ (string->number incre) 1)))))
+  (cond [(eq? opt 'add) (lambda (name obj)
+                          (if [find-node? entries name (lambda (x) (list-ref x 1))]
+                              (error "对象已经存在: " name)
+                              (set! entries [append-linkedlist entries (mcons (list address name obj) null)])))]
+        [(eq? opt 'get) (lambda (name)
+                          (get-element-by-value entries name (lambda (x) (list-ref x 1))))]))
 
 (define (add node)
   (cond [(and [not (mpair? [mcar node])] [not (mpair? [mcdr node])])
@@ -38,6 +54,11 @@
          (/ [calc (mcar node)] [mcdr node])]
         [(mpair? [mcdr node])
          (/ [mcar node] [calc (mcdr node)])]))
+;;; name: 对象名字
+;;; obj: 对象实体
+;;; table: 环境对象表
+(define (mydefine name obj table)
+  ((table 'add) name obj))
 
 (define procedures null)
 (set! procedures (append-linkedlist procedures [mcons (mcons [quote +] add) null]))
