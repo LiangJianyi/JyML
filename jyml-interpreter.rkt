851c30172e185fe14dfede81753ba83c57e06ecf
梁建溢 <a.124p@live.com>
2017-12-29 20:43:32 +0800

    Changesets-34

--- jyml-interpreter.rkt
+++ jyml-interpreter.rkt
@@ -2,21 +2,9 @@
 (require liangjianyi-racket/linkedlist)
 (require liangjianyi-racket-sundry/utility)
 (require "./jyml-parser.rkt")
+(require "./exec-enviroment.rkt")
+(require parser-tools/lex)
 
-(define (object-table opt enviroment)
-  (define address ((enviroment 'eid)))
-  (define entries null)
-  (define (address-incre!)
-    (let ([incre (substring (+ (string-first-index address #\|) 1)
-                            (string-length address))]
-          [area-code (substring address 0 (+ (string-first-index address #\|) 1))])
-      (set! address (string-append area-code (+ (string->number incre) 1)))))
-  (cond [(eq? opt 'add) (lambda (name obj)
-                          (if [find-node? entries name (lambda (x) (list-ref x 1))]
-                              (error "对象已经存在: " name)
-                              (set! entries [append-linkedlist entries (mcons (list address name obj) null)])))]
-        [(eq? opt 'get) (lambda (name)
-                          (get-element-by-value entries name (lambda (x) (list-ref x 1))))]))
 
 (define (add node)
   (cond [(and [not (mpair? [mcar node])] [not (mpair? [mcdr node])])
@@ -54,12 +42,12 @@
          (/ [calc (mcar node)] [mcdr node])]
         [(mpair? [mcdr node])
          (/ [mcar node] [calc (mcdr node)])]))
-;;; name: 对象名字
-;;; obj: 对象实体
-;;; table: 环境对象表
-(define (mydefine name obj table)
-  ((table 'add) name obj))
 
+
+;;; 判断一个数据是不是过程就看它是否在 procedures 表中存在
+;;; 在表中登记的数据（关键字）视为 procedure，其类型为 symbol
+;;; 当一个对象赋值为 lambda，那么该对象名就需要在表中登记
+;;;
 (define procedures null)
 (set! procedures (append-linkedlist procedures [mcons (mcons [quote +] add) null]))
 (set! procedures (append-linkedlist procedures [mcons (mcons [quote -] sub) null]))
@@ -80,7 +68,74 @@
         [(mpair? left) (calc [mcar left]) (calc [mcdr left])]))
 
 
-(define test-exp-1
-  (mcons '* (mcons (mcons '+ (mcons (mcons '/ (mcons 4 (mcons '- (mcons 100 98)))) 10)) 12)))
-(define test-exp-2
-  (mcons '+ (mcons (mcons '/ (mcons 100 10)) (mcons '* (mcons 2 5)))))
+;;; name: 对象名字
+;;; obj: 对象实体
+;;; table: 环境对象表
+(define (mydefine name obj table)
+  ((table 'add) name obj))
+
+(define (inferred-type node)
+  (define number-collection (string->linkedlist "0123456789"))
+  (define point #\.)
+  (define minus-mark #\-)
+  (define quote #\')
+  (define double-quote #\")
+  (define backslash #\\)
+  (define boolean-collection (linkedlist "true" "false" "#t" "#f"))
+  
+  (define (build-number node)
+    (define len (string-length node))
+    ;;; 下面的 cond 完全可以通过 string->number 和异常处理来达到同样的目的，只不过我自己重新造了轮子罢了
+    (cond [;;; 负号开头的分析过程
+           (equal? (string-ref node 0) minus-mark)
+           (cond [(= len 1) (error "类型无法识别: " node)]
+                 [(or (= len 2) (= len 3)) (for ([e (substring node 1 len)])
+                                             (when (or [not (find-node? number-collection e)]
+                                                       [equal? point e])
+                                               (error "类型无法识别: " node)))
+                                           (string->number node)]
+                 [(>= len 4) (if [or (equal? point (string-ref node 1)) (equal? point (string-ref node (- len 1)))]
+                                 (error "类型无法识别: " node)
+                                 (let ([point-count 0]
+                                       [sub (substring node 1 len)])
+                                   (for ([e sub])
+                                     (if [> point-count 1]
+                                         (error "类型无法识别: " node)
+                                         (cond [(equal? point e) (set! point-count (+ point-count 1))]
+                                               [(not (find-node? number-collection e)) (error "类型无法识别: " node)])))
+                                   (string->number node)))])]
+          [;;; 数字开头的分析过程
+           (find-node? number-collection (string-ref node 0))
+           (cond [(<= len 2)
+                  (for ([e node])
+                    (when [not (find-node? number-collection e)]
+                      (error "类型无法识别: " node)))
+                  (string->number node)]
+                 [(>= len 3)
+                  (let ([point-count 0]
+                        [sub (substring node 1 (- len 1))])
+                    (for ([e sub])
+                      (if [> point-count 1]
+                          (error "类型无法识别" node)
+                          (cond [(equal? point e) (set! point-count (+ point-count 1))]
+                                [(not (find-node? number-collection e)) (error "类型无法识别: " node)])))
+                    (string->number node))])]))
+  
+  (define (string-boolean-mapping lex)
+    (cond [(equal? lex "true") true]
+          [(equal? lex "false") false]
+          [(equal? lex "#t") #t]
+          [(equal? lex "#f") #f]
+          (error "类型无法识别: " lex)))
+  
+  
+  [cond [(find-node? boolean-collection node) (string-boolean-mapping node)]
+        [(and (equal? double-quote [string-ref node 0])
+              (equal? double-quote [string-ref node (- [string-length node] 1)])) 'string]
+        [(find-node? procedures (string->symbol node)) (string->symbol "procedure")]
+        [(equal? node "null") null]
+        [else (cond [(equal? (string-ref node 0) quote) (string->symbol node)]
+                    [(or (equal? (string-ref node 0) minus-mark)
+                         (find-node? number-collection (string-ref node 0)))
+                     (build-number node)]
+                    [else (error "类型无法识别: " node)])]])
