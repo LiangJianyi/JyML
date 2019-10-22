94c9d5fa38ee76b5439ecc3e121da65e94a253ef
梁建溢 <liangjianyi.com@outlook.com>
2019-10-22 01:26:15 -0500

    Changesets-55

---
+++ 一个简陋的实现/Sucker/Interpreter/JymlEnviroment.cs
@@ -0,0 +1,34 @@
+﻿using JymlAST;
+using JymlTypeSystem;
+using System;
+using System.Collections.Generic;
+
+namespace Interpreter {
+    public class JymlEnviroment {
+        public class Restraint {
+            public string Variable { get; set; }
+            public JymlType Value { get; set; }
+            public Restraint(string var, JymlType val) {
+                Variable = var;
+                Value = val;
+            }
+        }
+        public class Frame {
+            private LinkedList<Restraint> Restraints = new LinkedList<Restraint>();
+            public Frame(LinkedList<Restraint> restraints) => Restraints = restraints;
+            public void AddBidingToFrame(string var, JymlType val) => this.Restraints.AddFirst(new Restraint(var, val));
+        }
+        public Cons ExtendEnvironment(string[] variables, JymlType[] values, JymlEnviroment baseEnv) {
+            if (variables.Length == values.Length) {
+                LinkedList<Restraint> restraints = new LinkedList<Restraint>();
+                for (int i = 0; i < variables.Length; i++) {
+                    restraints.AddFirst(new Restraint(variables[i], values[i]));
+                }
+                return new Cons(new Frame(restraints), baseEnv);
+            }
+            else {
+                throw new Exception("变量名的数量与其值的数量不匹配。");
+            }
+        }
+    }
+}
--- 一个简陋的实现/Sucker/Interpreter/SuckerScriptInterpreter.cs
+++ 一个简陋的实现/Sucker/Interpreter/SuckerScriptInterpreter.cs
@@ -2,9 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using JymlAST;
+using JymlParser;
+using JymlTypeSystem;
 
 namespace Interpreter {
-    class SuckerScriptInterpreter {
+    static class SuckerScriptInterpreter {
+        public static Cons Eval(Cons exp, JymlEnviroment env) {
+            if (Parser.IsSelfEvaluating(exp)) {
+                return exp;
+            }
+            else if (Parser.IsVariable(exp)) {
+                return new Cons(env.Frame[exp]);
+            }
+            else if (Parser.IsAssignment(exp)) {
+                return EvalAssignment(exp, env);
+            }
+            else if (Parser.IsDefinition(exp)) {
+                return EvalDefinition(exp, env);
+            }
+            else if (Parser.IsIf(exp)) {
+                return EvalIf(exp, env);
+            }
+            else if (Parser.IsLambda(exp)) {
+                return MakeLambda(Parser.GetLambdaParameters(exp), Parser.GetLambdaBody(exp), env);
+            }
+            else if (Parser.IsBegin(exp)) {
+                return EvalSequence(BeginActions(exp));
+            }
+        }
     }
 }
